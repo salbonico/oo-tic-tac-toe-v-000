c35566e2a03a3fe4e59c79b732d9a1a7d48e08f6
Steve Albonico <salbonico@gmail.com>
2019-01-12 23:18:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,14 @@ else turn
 end
 end
 
-
+def turn_count(board)
+  @count = 0
+  @board.each do |position|
+    if position == "X"|| position == "O"
+      @count += 1
+    end
+  end
+  end
 
 
 end
