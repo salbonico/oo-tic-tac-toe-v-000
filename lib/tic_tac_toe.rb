fd644407a74220dc318e8341d83fec4750b0d493
Steve Albonico <salbonico@gmail.com>
2019-01-12 23:11:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,25 +7,16 @@ end
 WIN_COMBINATIONS =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def display_board
-
 puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-
 puts "-----------"
-
 puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-
 puts "-----------"
-
 puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-
 end
 
 def input_to_index(input)
-
 index = input.to_i
-
 @index = index - 1
-
 end
 
 def move(xo)
@@ -40,15 +31,17 @@ end
 def valid_move?
 
 if @index <0 || @index >8
-
 return false
-
 elsif
 @board[@index] == " "
-
 return true
 else return false
 end
 end
+
+
+
+
+
 
 end
